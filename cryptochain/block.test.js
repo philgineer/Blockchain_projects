9b5320d51d4b6ec03e46d9cafa64b6988483a501
Philgineer <43261253+yunjh0420@users.noreply.github.com>
2020-06-30 04:00:39 +0900

    update

--- cryptochain/block.test.js
+++ cryptochain/block.test.js
@@ -7,11 +7,15 @@ describe("Block", () => {
   const lastHash = "foo-hash";
   const hash = "bar-hash";
   const data = ["blockchain", "data"];
+  const nonce = 1;
+  const difficulty = 1;
   const block = new Block({
     timestamp,
     lastHash,
     hash,
     data,
+    nonce,
+    difficulty,
   });
 
   it("has a timestamp, lastHash, hash and data property", () => {
@@ -19,6 +23,8 @@ describe("Block", () => {
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+    expect(block.nonce).toEqual(nonce);
+    expect(block.difficulty).toEqual(difficulty);
   });
 
   describe("genesis()", () => {
@@ -56,7 +62,19 @@ describe("Block", () => {
 
     it("creats a SHA-256 `hash` based on the proper inputs", () => {
       expect(minedBlock.hash).toEqual(
-        cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
+        cryptoHash(
+          minedBlock.timestamp,
+          minedBlock.nonce,
+          minedBlock.difficulty,
+          lastBlock.hash,
+          data
+        )
+      );
+    });
+
+    it("sets a `hash` that matches the difficulty criteria", () => {
+      expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual(
+        "0".repeat(minedBlock.difficulty)
       );
     });
   });
--- cryptochain/blockchain.js
+++ cryptochain/blockchain.js
@@ -15,13 +15,28 @@ class Blockchain {
     this.chain.push(newBlock);
   }
 
+  replaceChain(chain) {
+    if (chain.lenth <= this.chain.length) {
+      console.error("The incoming chain must be longer");
+      return;
+    }
+
+    if (!Blockchain.isValidChain(chain)) {
+      console.error("The incoming chain must be valid");
+      return;
+    }
+
+    console.log("replacing chain with", chain);
+    this.chain = chain;
+  }
+
   static isValidChain(chain) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
       return false;
     }
 
     for (let i = 1; i < chain.length; i++) {
-      const { timestamp, lastHash, hash, data } = chain[i];
+      const { timestamp, lastHash, hash, data, nonce, difficulty } = chain[i];
 
       const actualLastHash = chain[i - 1].hash;
 
@@ -29,7 +44,13 @@ class Blockchain {
         return false;
       }
 
-      const validatedHash = cryptoHash(timestamp, lastHash, data);
+      const validatedHash = cryptoHash(
+        timestamp,
+        lastHash,
+        data,
+        nonce,
+        difficulty
+      );
 
       if (hash !== validatedHash) {
         return false;
